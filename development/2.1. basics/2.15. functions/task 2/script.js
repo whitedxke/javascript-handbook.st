24b7d9fcc76ba742ccfa2c0507189607bccb1998
eextjcxltuires <whitedxke@gmail.com>
2025-03-17 04:05:35 +0300

    Solution for the tasks from chapter 2.15.

---
+++ development/2.1. basics/2.15. functions/task 2/script.js
@@ -0,0 +1,11 @@
+// Напишите функцию min(a,b), которая возвращает меньшее из чисел a и b.
+
+("use strict");
+
+function min(a, b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
+}
